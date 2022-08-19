729619ef4faebe25702a0797ff0a74ad7923390c
TangAndClear <111523112+TangAndClear@users.noreply.github.com>
2022-08-19 14:49:33 -0400

    Update chap9.js

--- chap9.js
+++ chap9.js
@@ -1,5 +1,19 @@
 function loadXML() {
     const xmlhttp = new XMLHttpRequest()
     xmlhttp.open('GET', 'books.xml', true)
-    xmlhttp.sen
+    xmlhttp.send()
+    xmlhttp.onreadystatechange = function() {
+        if( this.readyState == 4 && this.status == 200)
+        getData(this)
+    }
+}
+
+function getData(xml) {
+    const xmlDoc = xml.responseXML
+    const tags = xmlDoc.getElementsByTagName('book')
+    let list = '', i = 0
+    for( i = 0; i < tags.length; i++){
+        list += '<li>' + tags[i].getElementsByTagName('title')[0].firstChild.data 
+    }
+    document.getElementById('list').innerHTML = list
 }
